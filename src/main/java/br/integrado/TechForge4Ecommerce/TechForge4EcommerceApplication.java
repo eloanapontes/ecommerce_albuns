22f5d32663449e85aed0d6c44d29e92ff28db986
eloanapontes <eloana.pontes@gmail.com>
2024-11-30 17:24:28 -0300

    Inicializando repositório do projeto TechForge4Ecommerce

---
+++ src/main/java/br/integrado/TechForge4Ecommerce/TechForge4EcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.integrado.TechForge4Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechForge4EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechForge4EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/integrado/TechForge4Ecommerce/TechForge4EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.integrado.TechForge4Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechForge4EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
